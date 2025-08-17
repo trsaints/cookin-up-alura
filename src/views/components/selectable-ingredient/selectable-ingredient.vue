111cfb068ee38377ff5c3d74e8fb9a98686e86bb
trsaints <thiago.dev2k3@gmail.com>
2025-08-17 11:34:58 -0300

    refactor: move component emits to new dedicated directory

--- src/views/components/selectable-ingredient/selectable-ingredient.vue
+++ src/views/components/selectable-ingredient/selectable-ingredient.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Tag from "../Tag.vue";
-import type { SelectableIngredientEmits } from "./emits";
+import type { SelectableIngredientEmits } from "../../emits/emits";
 
 const props = defineProps<{
   ingrediente: string;
