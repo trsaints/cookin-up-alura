319e83cd4b62ab698700d114d480218426ab8222
trsaints <thiago.dev2k3@gmail.com>
2025-08-17 12:46:10 -0300

    fix: enforce duplicate entries shouldn't be added to the selected ingredients list

--- src/views/models/main-content.vm.ts
+++ src/views/models/main-content.vm.ts
@@ -2,6 +2,8 @@ import type { IMainContentViewModel } from "./interfaces/imain-content.vm";
 
 export class MainContentViewModel implements IMainContentViewModel {
   addIngredient(newIngredient: string, ingredients: string[]): boolean {
+    if (ingredients.includes(newIngredient)) return false;
+
     ingredients.push(newIngredient);
 
     if (ingredients.includes(newIngredient)) return true;
