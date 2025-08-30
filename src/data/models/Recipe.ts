0f4cf82982b6944dd52dc437891f909c9b309eec
trsaints <thiago.dev2k3@gmail.com>
2025-08-30 13:58:25 -0300

    feat: provide both Recipe type alias and 'recipes-selection' component props

---
+++ src/data/models/Recipe.ts
@@ -0,0 +1,5 @@
+export type Recipe = {
+  nome: string;
+  ingredientes: string[];
+  imagem: string;
+};
---
+++ src/views/props/recipes-selection-props.ts
@@ -0,0 +1,5 @@
+import type { Recipe } from "@/data/models/Recipe";
+
+export type RecipesSelectionProps = {
+  recipes: Recipe[];
+};
