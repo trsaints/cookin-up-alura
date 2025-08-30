c1e658c4ea2e1569b87ba4bf881f1e48f8d0cdd1
trsaints <thiago.dev2k3@gmail.com>
2025-08-30 12:42:08 -0300

    feat: create footer component for providing the base application's layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Banner />
   <ConteudoPrincipal />
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Banner from "./views/components/banner/banner.vue";
 import ConteudoPrincipal from "./views/components/ConteudoPrincipal.vue";
+import Footer from "./views/components/footer/footer.vue";
 </script>
---
+++ src/views/components/footer/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <p>
+      Desenvolvido Por @Trsaints | {{ new Date().getFullYear() }} - Projeto
+      fictício e sem fins comerciais.
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 48px 96px;
+}
+</style>
