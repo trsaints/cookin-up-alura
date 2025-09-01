9a53e2f5afc615b7c94281b4a5d09263983ff6da
trsaints <thiago.dev2k3@gmail.com>
2025-08-31 21:07:05 -0300

    feat: provide method for filtering Recipes per 'ingredients' field

--- src/server/services/interfaces/irecipe-service.ts
+++ src/server/services/interfaces/irecipe-service.ts
@@ -2,4 +2,5 @@ import type { Recipe } from "@/data/models/Recipe";
 
 export interface IRecipeService {
   getAll(): Promise<Recipe[]>;
+  byIngredients(recipes: Recipe[], ingredients: string[]): Recipe[];
 }
--- src/server/services/recipe-serivce.ts
+++ src/server/services/recipe-serivce.ts
@@ -2,6 +2,12 @@ import type { Recipe } from "@/data/models/Recipe";
 import type { IRecipeService } from "./interfaces/irecipe-service";
 
 export class RecipeService implements IRecipeService {
+  byIngredients(recipes: Recipe[], ingredients: string[]): Recipe[] {
+    return recipes.filter((r) =>
+      r.ingredientes.every((i) => ingredients.includes(i))
+    );
+  }
+
   async getAll(): Promise<Recipe[]> {
     const response = await fetch(
       "https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json"
