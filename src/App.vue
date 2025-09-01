b3984f2f7cbe8a56418c8b13c1ae3296456aab25
trsaints <thiago.dev2k3@gmail.com>
2025-08-31 21:49:18 -0300

    refactor: rename component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <Banner />
-  <ConteudoPrincipal />
+  <MainContent />
   <Footer />
 </template>
 
 <script setup lang="ts">
-import Banner from "./views/components/banner/banner.vue";
-import ConteudoPrincipal from "./views/components/ConteudoPrincipal.vue";
-import Footer from "./views/components/footer/footer.vue";
+import Banner from "@/views/components/banner/banner.vue";
+import Footer from "@/views/components/footer/footer.vue";
+import MainContent from "@/views/components/main-content.vue";
 </script>
